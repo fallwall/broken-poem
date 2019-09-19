28abaa603bb4bb29f186534dec7da73ede23c474
XX <fallwall19@gmail.com>
2019-09-19 00:08:44 -0400

    TSX on client

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <p>Never fear, TSX is here.</p>
+    </div>
+  );
+}
+
+export default App;
