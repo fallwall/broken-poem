f1a48b7e0996e87a84bf3f7f50ef571b0b0c23c0
XX <fallwall19@gmail.com>
2019-09-25 14:28:27 -0400

    strictNullChecks: false

--- client/src/components/Parts.tsx
+++ client/src/components/Parts.tsx
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
 import Part from '../interfaces/Part.interface';
-import { number } from 'prop-types';
 
-
+interface SearchState { 
+  id: number,
+  word: string
+}
 export class Parts extends Component<Part> {
+  partRef: React.RefObject<HTMLInputElement>;
+  constructor(props: Part) { 
+    super(props);
+    this.state = {
+      word: '',
+      id: 0
+    }
+    this.partRef = React.createRef();
+  }
+
+  onSearchClick = () => { 
+    const inputValue = this.partRef.current.value;
+  }
+
   render() {
     const { id, word } = this.props;
     return (
       <div>
         <p>ID: {id}</p>
         <p>Word: {word}</p>
+        <input type="text" ref={this.part} />
+        <button
+          onClick={this.onSearchClick}
+          className="search-button">
+          Search
+        </button>
       </div>
     )
   }
