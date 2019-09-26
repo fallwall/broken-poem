627a64abf84919c03a75ee596ba908c26bd6606b
XX <fallwall19@gmail.com>
2019-09-26 11:59:06 -0400

    interface+searchstate

--- client/src/components/Parts.tsx
+++ client/src/components/Parts.tsx
@@ -2,26 +2,58 @@ import React, { Component } from 'react';
 import Part from '../interfaces/Part.interface';
 
 interface SearchState { 
-  id: number,
-  word: string
+  error: boolean;
+  part: Part;
 }
-export class Parts extends Component<Part> {
+
+// interface Part { 
+//   id: number;
+//   word: string;
+// }
+
+export class Parts extends Component<Part, SearchState> {
   partRef: React.RefObject<HTMLInputElement>;
   constructor(props: Part) { 
     super(props);
     this.state = {
-      word: '',
-      id: 0
+      error: false,
+      part: null
     }
     this.partRef = React.createRef();
   }
 
   onSearchClick = () => { 
     const inputValue = this.partRef.current.value;
+    fetch(`http://localhost:3000/search/${inputValue}`)
+      .then(res => { 
+        if (res.status !== 200) { 
+          this.setState({ error: true });
+          return;
+        }
+        res.json().then(data => { 
+          this.setState({
+            error: false,
+            part: {
+              id: data.id,
+              word: data.word
+            }
+          })
+        })
+      })
   }
 
   render() {
-    const { id, word } = this.props;
+    const { id, word} = this.props;
+    const { error, part } = this.state;
+
+    let resultMarkup;
+
+    if (error) {
+      resultMarkup = <p>not found</p>;
+    } else if (part) {
+      resultMarkup = <div>{part.id}, {part.word}</div>;
+    }
+
     return (
       <div>
         <p>ID: {id}</p>
@@ -32,6 +64,7 @@ export class Parts extends Component<Part> {
           className="search-button">
           Search
         </button>
+        {resultMarkup}
       </div>
     )
   }
